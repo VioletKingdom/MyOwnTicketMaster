fcb888dfc11f7594dfcc6c3170819fdcc8e835ac
VioletKingdom <55386758+VioletKingdom@users.noreply.github.com>
2019-09-26 16:29:14 -0700

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -88,4 +88,40 @@ public interface DBConnection {
 	 * @return boolean
 	 */
 	public boolean verifyLogin(String userId, String password);
+	
+	
+	/**
+	 * Register one user
+	 * 
+	 * @param userId
+	 * @param password
+	 * @param firstname
+	 * @param lastname
+	 * @return boolean
+	 */
+	public boolean registerUser(String userId, String password, String firstname, String lastname) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return false;
+		}
+
+		try {
+			String sql = "INSERT IGNORE INTO users VALUES (?, ?, ?, ?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			ps.setString(2, password);
+			ps.setString(3, firstname);
+			ps.setString(4, lastname);
+			
+			return ps.executeUpdate() == 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;	
+	}
+
+}
+
+
+
 }
