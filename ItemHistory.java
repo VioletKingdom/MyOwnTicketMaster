8175b0d77c1126cfd92d5090229fcaf88851b161
VioletKingdom <55386758+VioletKingdom@users.noreply.github.com>
2019-09-26 16:14:44 -0700

    Update ItemHistory.java

--- ItemHistory.java
+++ ItemHistory.java
@@ -1,8 +1,20 @@
 package rpc;
 
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class ItemHistory extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	/**
               * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -45,6 +57,54 @@ public class ItemHistory extends HttpServlet {
   		 connection.close();
   	 }
    }
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// default line
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+			String userId = request.getParameter("user_id");
+		JSONArray array = new JSONArray();
+		
+		DBConnection conn = DBConnectionFactory.getConnection();
+		try {
+			Set<Item> items = conn.getFavoriteItems(userId);
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				obj.append("favorite", true);
+				array.put(obj);
+			}
+			
+			RpcHelper.writeJsonArray(response, array);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}		
+		// use close to close the writer
+		// this can set free some resource and it is a good habit
+		writer.close();
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
+
+
